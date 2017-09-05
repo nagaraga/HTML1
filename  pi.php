db36f4bf000561f5a2e0694a6e6429fdc13dddb3
nagaraga <31654549+nagaraga@users.noreply.github.com>
2017-09-05 16:47:54 +0530

    Add files via upload

---
+++  pi.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:powderblue"></body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><center>
+
+<img src="file:///home/greefi/Desktop/sss.jpeg" alt="error in loading" style="width:250px;height:250px">
+<h1>Hi..babloo</h1>
+</center>
